3edcd02a2db07a15ea4a308ade321630fa9c1b5e
lav7n <lavensri@gmail.com>
2024-03-29 17:59:41 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,6 @@ from .iter_counter import IterationCounter
 from .visualizer import Visualizer
 from .metric_tracker import MetricTracker
 from .metrics import mean_dice
-from .distribution_estimation import kmeans, pairwise_cosine
+# from .distribution_estimation import kmeans, pairwise_cosine
 from .masking import FixedThresholding,SoftMatchWeighting
 from .dist_align import DistAlignEMA
